390def247d59ca6017b6d401ec50156fba84a2c8
mikhailstasyuk <78797945+mikhailstasyuk@users.noreply.github.com>
2024-09-16 17:57:44 +0000

    Edited to create url programmatically

--- app/database.py
+++ app/database.py
@@ -1,5 +1,6 @@
 import logging
 from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
 from app.config import Config
 
 config = Config.load_config()
@@ -13,18 +14,26 @@ else:
 logging.basicConfig(level=log_level)
 logger = logging.getLogger(__name__)
 
-def test_db() -> None:
+def connect() -> tuple:
     DB_NAME = config['db_name']
     DB_PORT = config['db_port']
     DB_USER = config['db_user']
     DB_PASSWORD = config['db_password']
 
-    DB_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@postgres:{DB_PORT}/{DB_NAME}'  
-    engine = create_engine(DB_URI)
+    url = URL.create(
+        drivername="postgresql",
+        username=DB_USER,
+        password=DB_PASSWORD,
+        host="postgres",
+        database="postgres"
+)
+
+    engine = create_engine(url)
+
     try:
         with engine.connect() as connection:
             logger.info("Connection to the database was successful!")
-            return True
+            return True, connection
     except Exception as e:
         logger.error(f"Error connecting to the database: {e}")
-        return False
+        return False, None
