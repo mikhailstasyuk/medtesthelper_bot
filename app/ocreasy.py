6931d2aed536cae3a86a12016e6dea3b001421b4
mikhailstasyuk <78797945+mikhailstasyuk@users.noreply.github.com>
2024-09-17 20:27:38 +0000

    Update requirements.txt

--- app/ocreasy.py
+++
@@ -1,8 +0,0 @@
-import easyocr
-reader = easyocr.Reader(['ru', 'en'])
-
-result = reader.readtext('../images/ivanov.png')
-
-for (bbox, text, prob) in result:
-    print(f'Text: {text}, Probability: {prob}')
-
