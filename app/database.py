5cf762712f4d80138ab5b43e5da954208e7d224d
mikhailstasyuk <78797945+mikhailstasyuk@users.noreply.github.com>
2024-09-19 19:28:56 +0000

    Add medical study data format support

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from datetime import date
 import logging
-from typing import List, Dict, Any
+from typing import Union, List, Dict, Any
 
 from sqlalchemy import create_engine
 from sqlalchemy import desc
@@ -8,14 +8,17 @@ from sqlalchemy.engine import URL
 from sqlalchemy.orm import Session, sessionmaker
 
 from app.config import Config
-from app.document_parse import DataEntry, Document
+from app.document_parse import (
+    MedTestDataEntry, MedStudyDataEntry, Document
+)
 from app.schema import (
     User, MedicalInstitution, 
-    MedicalDocument, MedicalData, create_tables
+    MedicalDocument, TestData, StudyData, create_tables
 )
 
 
 config = Config.load_config()
+
 
 log_level_str = config['log_level']
 if log_level_str: 
@@ -23,12 +26,18 @@ if log_level_str:
 else: 
     log_level = logging.getLevelName(logging.INFO)
 
-logging.basicConfig(level=log_level)
+logging.basicConfig(
+    filename='log.log',
+    filemode='a',  
+    level=logging.DEBUG,     
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'  
+)
+
 logger = logging.getLogger(__name__)
 
 
 def create_database_url():
-    """Programmatically construct database URL"""
+    """Programmatically construct database URL."""
     url = URL.create(
         drivername='postgresql',
         username=config['db_user'],
@@ -48,7 +57,7 @@ def create_database_engine():
 
 
 def create_database_tables():
-    """Create database tables according to schema"""
+    """Create database tables according to schema."""
     engine = create_database_engine()
     try:
         create_tables(engine)    
@@ -65,17 +74,16 @@ def add_medical_document(
     institution_name: str, 
     document_type: str,
     document_date: date, 
-    data_entries: DataEntry
+    data_format: str,
+    data_entries: List[Union[MedTestDataEntry, MedStudyDataEntry]]
 ):
-    """Add user's medical document to the database"""
-    user = session.query(User).filter_by(
-        telegram_id=telegram_id).first()
+    """Add user's medical document to the database."""
+    user = session.query(User).filter_by(telegram_id=telegram_id).first()
     if not user:
         user = User(telegram_id=telegram_id)
         session.add(user)
 
-    institution = session.query(MedicalInstitution).filter_by(
-        name=institution_name).first()
+    institution = session.query(MedicalInstitution).filter_by(name=institution_name).first()
     if not institution:
         institution = MedicalInstitution(name=institution_name)
         session.add(institution)
@@ -87,80 +95,105 @@ def add_medical_document(
         document_date=document_date
     )
     session.add(document)
-    
-    for entry in data_entries:
-        medical_data = MedicalData(
-            document=document,
-            field_name=entry.name,
-            field_value=entry.value,
-            unit=entry.unit,
-            reference_range=entry.ref_range,
-            is_normal=entry.is_normal
-        )
-        session.add(medical_data)
+
+    # Insert data based on the data_format flag
+    if data_format == 'test':
+        for entry in data_entries:
+            test_data = TestData(
+                document=document,
+                name=entry.name,
+                value=entry.value,
+                unit=entry.unit,
+                range=entry.ref_range,
+                commentary=entry.commentary
+            )
+            session.add(test_data)
+    elif data_format == 'study':
+        for entry in data_entries:
+            study_data = StudyData(
+                document=document,
+                device=entry.device,
+                result=entry.result,
+                report=entry.report,
+                recommendation=entry.recommendation
+            )
+            session.add(study_data)
     
     session.commit()
 
 
-def fetch_latest_medical_data(session: Session, telegram_id: int, 
-        document_type: str = None):
+def fetch_latest_medical_data(session: Session, telegram_id: int, document_type: str = None):
     """Fetch latest medical data for the user"""
-    user = session.query(User).filter_by(
-        telegram_id=telegram_id).first()
+    user = session.query(User).filter_by(telegram_id=telegram_id).first()
 
     if not user:
         return None
     
-    query = session.query(MedicalDocument).filter(
-        MedicalDocument.user_id == user.user_id)
+    query = session.query(MedicalDocument).filter(MedicalDocument.user_id == user.user_id)
     
     if document_type:
-        query = query.filter(
-            MedicalDocument.document_type == document_type)
+        query = query.filter(MedicalDocument.document_type == document_type)
     
-    latest_document = query.order_by(
-        desc(MedicalDocument.document_date)).first()
+    latest_document = query.order_by(desc(MedicalDocument.document_date)).first()
     
     if not latest_document:
         return None  
     
-    data = session.query(MedicalData).filter(
-        MedicalData.document_id == latest_document.document_id).all()
-    
-    result = {
+    # Initialize result without data_format
+    fetched = {
         "document_type": latest_document.document_type,
         "document_date": latest_document.document_date,
         "institution": latest_document.institution.name,
-        "data": [{
-            "field_name": item.field_name,
-            "field_value": item.field_value,
-            "unit": item.unit,
-            "reference_range": item.reference_range,
-            "is_normal": item.is_normal
-        } for item in data]
+        "data": []
     }
-    return result
+
+    # Fetch test or study data based on the actual data present
+    test_data = session.query(TestData).filter(TestData.document_id == latest_document.document_id).all()
+    if test_data:
+        fetched["data_format"] = 'test'
+        fetched["data"] = [{
+            "name": item.name,
+            "value": item.value,
+            "unit": item.unit,
+            "range": item.range,
+            "commentary": item.commentary
+        } for item in test_data]
+    else:
+        study_data = session.query(StudyData).filter(StudyData.document_id == latest_document.document_id).all()
+        if study_data:
+            fetched["data_format"] = 'study'
+            fetched["data"] = [{
+                "device": item.device,
+                "result": item.result,
+                "report": item.report,
+                "recommendation": item.recommendation
+            } for item in study_data]
+
+    return fetched
 
 
 def add_and_fetch(telegram_id, document_json):
     engine = create_database_engine()
     Session = sessionmaker(bind=engine)
     document = Document.from_json(document_json)
-
     with Session() as session:
-        telegram_id = telegram_id
+        session.expire_all()
         add_medical_document(
             session,
-            telegram_id=telegram_id,  
+            telegram_id=telegram_id,
             institution_name=document.institution_name,
             document_type=document.document_type,
-            document_date=date.fromisoformat(document.document_date),
+            document_date=document.document_date,
+            data_format=document.data_format,
             data_entries=document.data
         )
     
-        result = fetch_latest_medical_data(session, telegram_id, document_type="Клинический анализ крови")
-        if result:
-            data = result['data'][0]
-            return f"{data['field_name']}: {data['field_value']} {data['unit']} (Normal: {data['is_normal']})"
+        fetched = fetch_latest_medical_data(session, telegram_id, document_type=document.document_type)
+        if fetched:
+            data = fetched['data'][0]
+            if fetched['data_format'] == 'test':
+                return f"{data['name']}: {data['value']} {data['unit']} (Commentary: {data['commentary']})"
+            elif fetched['data_format'] == 'study':
+                return f"Device: {data['device']}, Recommendation: {data['recommendation']}"
         else:
             return "No data found for this user."
